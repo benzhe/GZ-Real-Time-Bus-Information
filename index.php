ac4336ec1d81a96b7844b92ce9be97d77fddee66
ben304zhe@gmail.com <ben304zhe@gmail.com>
2012-05-22 17:37:16 +0800

    clean

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
     'timeout'=>30, 
   )); 
   $context = stream_context_create($opts); 
-  //$html =file_get_contents('http://www.example.com', false, $context); 
 
   $action = empty($_GET['a'])?'':$_GET['a'];
   $re = !empty($_GET['s'])?strtoupper($_GET['s']):'';
@@ -64,16 +63,16 @@
         $route_list = explode(',', $obj_result->content);
         //var_dump($route_list);
         if(count($route_list) > 1) {
-	  echo '<table border="1" class="search_result">';
-	  echo '<tr><th>请选择线路：</th></tr>';
-	  foreach($route_list as $k => $v) {
-	    echo '<tr><td><a href="?a=detail&s='.$v.'" >'.$v.'</a></td></tr>';
-	  }
-	  echo '</table>';
-	}
-	else {
-	  $route = $route_list[0];
-	}
+    echo '<table border="1" class="search_result">';
+    echo '<tr><th>请选择线路：</th></tr>';
+    foreach($route_list as $k => $v) {
+      echo '<tr><td><a href="?a=detail&s='.$v.'" >'.$v.'</a></td></tr>';
+    }
+    echo '</table>';
+  }
+  else {
+    $route = $route_list[0];
+  }
       }
 
     elseif($action == 'detail') {
@@ -86,12 +85,12 @@
       $raw_result = @file_get_contents($detail_url.iconv('utf-8','gb2312',strtoupper($route)),false,$context);
       //var_dump($raw_result);
       $obj_result = json_decode($raw_result);
-	  //var_dump($obj_result);
+    //var_dump($obj_result);
       if(empty($obj_result) || $obj_result->statusCode != -1) err('-4');
       $r = $obj_result;
       $lines = array();
       foreach($r->content as $k) {
-		array_push($lines,$k);
+    array_push($lines,$k);
       } 
       //var_dump($lines);
     ?>  
@@ -99,7 +98,7 @@
     <p><small>可将此页设为书签；点击车辆可两小时内跟踪</small></p>
     <table border ="1">
       <?php foreach($lines as $k => $v) { 
-	$station_names = explode(',', $v->busLine->stationNames);
+  $station_names = explode(',', $v->busLine->stationNames);
         //var_dump($station_names);
         $stations = array();
         foreach($station_names as $key => $val) {
@@ -116,17 +115,17 @@
         //var_dump($stations);
       ?>
       <tr class="title"><th><?php if($v->busLine->lineDirection == 0) { ?>
-	  上行&nbsp;&dArr;
-	    <?php } elseif($v->busLine->lineDirection == 1) {?>
-	  下行&nbsp;&dArr;
-	  <?php } else echo $v->busLine->lineDirection ?>
+    上行&nbsp;&dArr;
+      <?php } elseif($v->busLine->lineDirection == 1) {?>
+    下行&nbsp;&dArr;
+    <?php } else echo $v->busLine->lineDirection ?>
       </th><td>始发站：<?php echo $v->busLine->strPlatName ?>，终点站：<?php echo $v->busLine->endPlatName ?></td></tr>
       <?php foreach($stations as $st) { ?>
         <tr>
           <td><?php echo $st['name'] ?></td>
           <td>
       <?php foreach($st['bus'] as $bus) {
-		echo '<span name="b'.$bus->equipKey.'" onclick="Follow.follow('.$bus->equipKey.')">';
+    echo '<span name="b'.$bus->equipKey.'" onclick="Follow.follow('.$bus->equipKey.')">';
         echo $bus->equipKey.'号车在';
         $intervalo = date_diff(date_create($bus->inTime), date_create());
         $out = $intervalo->format("<b>&nbsp;%i分钟%s秒&nbsp;</b>前到达此站&nbsp;&dArr;");
@@ -138,40 +137,40 @@
       <?php } ?>
       <?php } ?>
     </table>
-	<script>
-	var timer = document.getElementById('timer');  var i = 25; var m = setInterval(function(){timer.innerHTML=i+'秒后自动';--i;if(i==0){clearInterval(m);location.reload();}},1000);
-	document.getElementById('s').value = '<?php echo strtoupper($route); ?>';
-	var Follow = {
-		list:[<?php echo @$_COOKIE['bus']; ?>],
-		follow:function(bus) {for(var i in this.list) {if(this.list[i]==bus) {this.unfollow(i,bus);return;}};this.list.push(bus);this.recookie();},
-		unfollow:function(bus_i,bus) {this.list.splice(bus_i,1);this.color(bus);this.recookie();},
-		recookie:function(){
-			this.color();
-			var exp  = new Date();
-			exp.setTime(exp.getTime() + 2*60*60*1000);
-			document.cookie = 'bus=' + escape(this.list.join(',')) + ";expires=" + exp.toString() + ';';		
-		},
-		color:function(bus){
-			if(bus) {
-					var items = document.getElementsByName('b'+bus);
-					for(var a = 0; a < items.length; ++a) {
-						items[a].style.color = '';
-					}		
-			}
-			else {
-				for(var i in this.list) {
-					var items = document.getElementsByName('b'+this.list[i]);
-					for(var a = 0; a < items.length; ++a) {
-						items[a].style.color = '#F00';
-					}
-				}
-			}
-		}
-	};
-	try{Follow.color();}catch(err){}
-	</script>
+  <script>
+  var timer = document.getElementById('timer');  var i = 25; var m = setInterval(function(){timer.innerHTML=i+'秒后自动';--i;if(i==0){clearInterval(m);location.reload();}},1000);
+  document.getElementById('s').value = '<?php echo strtoupper($route); ?>';
+  var Follow = {
+    list:[<?php echo @$_COOKIE['bus']; ?>],
+    follow:function(bus) {for(var i in this.list) {if(this.list[i]==bus) {this.unfollow(i,bus);return;}};this.list.push(bus);this.recookie();},
+    unfollow:function(bus_i,bus) {this.list.splice(bus_i,1);this.color(bus);this.recookie();},
+    recookie:function(){
+      this.color();
+      var exp  = new Date();
+      exp.setTime(exp.getTime() + 2*60*60*1000);
+      document.cookie = 'bus=' + escape(this.list.join(',')) + ";expires=" + exp.toString() + ';';    
+    },
+    color:function(bus){
+            if(bus) {
+              var items = document.getElementsByName('b'+bus);
+              for(var a = 0; a < items.length; ++a) {
+                      items[a].style.color = '';
+              }    
+            }
+          else {
+            for(var i in this.list) {
+              var items = document.getElementsByName('b'+this.list[i]);
+              for(var a = 0; a < items.length; ++a) {
+                items[a].style.color = '#F00';
+              }
+            }
+          }
+    }
+  };
+  try{Follow.color();}catch(err){}
+  </script>
     <?php
     }
-    ?>	
+    ?>  
   </body>
 </html>
